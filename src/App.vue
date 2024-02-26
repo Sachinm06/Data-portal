0df547946cf303205f98ee99006ee21aa6c492d7
sachin <sachinm06@aol.com>
2024-02-27 00:46:52 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HighChart></HighChart>
+  </div>
+</template>
+
+<script>
+import HighChart from './components/HighChart.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HighChart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HighChart.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <div id="chart-container"></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    mounted() {
+      // Ensure Highcharts is available globally
+      const Highcharts = require('highcharts');
+      require('highcharts/modules/exporting')(Highcharts);
+  
+      // Then, create your chart
+      Highcharts.chart('chart-container', {
+        chart: {
+          type: 'bar'
+        },
+        title: {
+          text: 'Sample Chart'
+        },
+        series: [{
+          name: 'Data',
+          data: [1, 2, 3, 4, 5]
+        }]
+      });
+    }
+  }
+  </script>
+  
